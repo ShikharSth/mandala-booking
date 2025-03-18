44d86a31f34b6eba7ec336f47a4af1b43d5966b0
shikharshrestha <shtshikhar12@gmail.com>
2025-03-18 11:40:19 +0545

    edit: deploy

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import EventDetails from './components/EventDetails.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router basename="/mandala-booking">
+    <Router basename="/mandala-booking/">
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
